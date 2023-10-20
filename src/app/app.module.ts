4eaf2116c0dacfb22cc82450ea8be3c9a7b49ee4
bruno kreppel <bruno.kreppel@gmail.com>
2023-10-20 10:37:38 +0200

    added project folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetailsComponent } from './details/details.component';
+import { OrderCartComponent } from './order-cart/order-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    MenuComponent,
+    DetailsComponent,
+    OrderCartComponent
   ],
   imports: [
     BrowserModule,
